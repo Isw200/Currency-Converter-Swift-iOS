c2c97f982e2a9b3d38bf1f5bf317e1de6d8568b0
Isuru Ariyarathna <isuruariyarathna2k00@gmail.com>
2024-11-06 11:56:20 +0530

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Isuru Ariyarathna on 2024-11-06.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
